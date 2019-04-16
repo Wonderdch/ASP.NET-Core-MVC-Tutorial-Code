cafce82f1dde07ad01c2d777b1e6720a05911695
Wonderdch <476681765@qq.com>
2019-04-16 16:50:59 +0800

    23 Finished

---
+++ Heavy.Web/ViewComponents/InternetStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Heavy.Web.ViewComponents
+{
+    public class InternetStatus : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://www.baidu.com");
+
+            return View(response.StatusCode == HttpStatusCode.OK);
+        }
+    }
+}
